04cdbb5b45db12cf85f8b8d424871981adb6b94b
LINDA ANGULO LOPEZ <43523986+lindangulopez@users.noreply.github.com>
2019-05-30 17:24:13 +0200

    Add files via upload

---
+++ Coursera_GitIt_Atom.py
@@ -0,0 +1,34 @@
+
+# coding: utf-8
+
+#
+# <h1 align="center"><font size="5">Classification with Python</font></h1>
+
+# In this notebook we load  we import the basic panda libraries,
+
+# print a greeting and
+
+# then share :D
+#
+# So, her we go:
+
+# In[1]:
+
+
+# @hidden_cell
+
+
+# In[2]:
+
+
+import numpy as np
+import pandas as pd
+
+# In[3]:
+
+print('Hello Capstone Project Course!')'
+
+
+# In[4]:
+#
+# <h4>Author:  <a href="www.linkedin.com/in/linda-angulo-lópez-881286a</a></h4>

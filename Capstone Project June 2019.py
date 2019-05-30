9bce5a52080abc749aa28c494de8ff67751f257e
LINDA ANGULO LOPEZ <43523986+lindangulopez@users.noreply.github.com>
2019-05-30 17:49:56 +0200

    Add files via upload

---
+++ Capstone Project June 2019.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### coding: utf-8
+# 
+# # <h1 align="center"><font size="5">Capstone Project Notebook</font></h1>
+# 
+# ### In this notebook we load  we import the basic panda libraries,
+# 
+# ### print a greeting and  then share and edit on Github :D
+# 
+# 
+# ## So, here we go:
+
+# In[2]:
+
+
+# @hidden_cell
+
+
+# In[3]:
+
+
+import numpy as np
+import pandas as pd
+
+
+# In[4]:
+
+
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# ### <h4>Author:  <a href="www.linkedin.com/in/linda-angulo-lópez-881286a</a></h4>
